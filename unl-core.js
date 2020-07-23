d8658225f3b8c495040b415172f7cad0511ff277
Cristian Calugar <cristian.calugar@rebeldot.com>
2020-07-23 14:11:49 +0300

    fix(unl-core): Fixed typo in JsDoc

--- unl-core.js
+++ unl-core.js
@@ -327,7 +327,7 @@ LocationId.appendElevation = function (
  * Returns grid lines for specified SW/NE latitude/longitude bounds and precision.
  *
  * @param   {sw: {lat: number, lon: number}, ne: {lat: number, lon: number}} bounds - The bound whithin to return the grid lines.
- * @param   {number} [precision] - Number of characters to conisder for the locationId of a grid cell.
+ * @param   {number} precision - Number of characters to consider for the locationId of a grid cell.
  * @returns {[[number, number],[number, number]][]}
  */
 LocationId.gridLines = function (bounds, precision) {
